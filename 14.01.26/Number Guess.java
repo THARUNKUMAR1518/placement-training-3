0227a5fa21d660e9234ad671cede069f673beb53
THARUN KUMAR R <151331105+THARUNKUMAR1518@users.noreply.github.com>
2026-01-14 11:46:05 +0530

    Add Number Guess game implementation

---
+++ 14.01.26/Number Guess.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class NG{
+    public static void main(String[] a){
+        int n=new Random().nextInt(10);
+        int g=5;
+        System.out.println(g==n?"Win":"Lose "+n);
+    }
+}
