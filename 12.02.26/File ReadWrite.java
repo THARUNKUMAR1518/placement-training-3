571b2a239da9f5fb98debda9ec798f7ad488888c
THARUN KUMAR R <151331105+THARUNKUMAR1518@users.noreply.github.com>
2026-01-14 11:44:08 +0530

    Add File ReadWrite.java for file operations

---
+++ 12.02.26/File ReadWrite.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class FH{
+    public static void main(String[] a)throws Exception{
+        FileWriter w=new FileWriter("a.txt");
+        w.write("Hello"); w.close();
+        BufferedReader r=new BufferedReader(new FileReader("a.txt"));
+        System.out.println(r.readLine());
+        r.close();
+    }
+}
